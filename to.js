d41e6036c37a3591686673808fb4a11ce015c434
GUDURU SRINIVASA REDDY <g.svreddyy@gmail.com>
2026-05-26 13:00:25 -0500

    Remove addTask function and add jQuery event handlers

--- to.js
+++ to.js
@@ -1,33 +1,4 @@
-function addTask() {
-    var taskInput = document.getElementById('taskInput');
-    var taskList = document.getElementById('taskList');
-    var task = taskInput.value;
-
-    if (task === '') {
-        alert('Please enter a task');
-        return;
-    }
-
-    var li = document.createElement('li');
-
-    var checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    checkbox.onclick = function() {
-        if (checkbox.checked) {
-            li.classList.add('completed');
-            taskList.appendChild(li); // Move to bottom
-        } else {
-            li.classList.remove('completed');
-            taskList.insertBefore(li, taskList.firstChild); // Move to top
-        }
-    };
-
-    var text = document.createElement('span');
-    text.textContent = task;
-
-    li.appendChild(checkbox);
-    li.appendChild(text);
-    taskList.insertBefore(li, taskList.firstChild);
-
-    taskInput.value = '';
-}
+$(document).ready(function() {
+    $('#list').click(function(event){event.preventDefault();$('#products .item').addClass('list-group-item');});
+    $('#grid').click(function(event){event.preventDefault();$('#products .item').removeClass('list-group-item');$('#products .item').addClass('grid-group-item');});
+});
